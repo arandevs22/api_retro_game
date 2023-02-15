54bab76d64f3e710d63c704f5e9bd9d9f3485ad0
Alfredo Cruz <arandevs22@gmail.com>
2023-02-15 15:28:54 -0600

    background commit

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const games = [
       "https://cdn2.steamgriddb.com/file/sgdb-cdn/thumb/29ab124d059e51600744303ec829069e.jpg",
     logo: "https://cdn2.steamgriddb.com/file/sgdb-cdn/logo_thumb/66410adefc38da1e9486483597c15518.png",
     background:
-      "https://cdn2.steamgriddb.com/file/sgdb-cdn/hero_thumb/20877befbd58c865e224346e6b92d772.jpg",
+      "https://cdn2.steamgriddb.com/file/sgdb-cdn/hero/20877befbd58c865e224346e6b92d772.jpg",
     favorite: false,
   },
   {
@@ -30,7 +30,7 @@ const games = [
       "https://cdn2.steamgriddb.com/file/sgdb-cdn/thumb/a7843ff15ebf9f9b7ceda39eb8b1f117.jpg",
     logo: "https://cdn2.steamgriddb.com/file/sgdb-cdn/logo_thumb/dcb8dc74a2a76e9e90526ddbfd759ba7.png",
     background:
-      "https://cdn2.steamgriddb.com/file/sgdb-cdn/hero_thumb/68c16354b766f0b3cb1c00299e08d485.jpg",
+      "https://cdn2.steamgriddb.com/file/sgdb-cdn/hero/68c16354b766f0b3cb1c00299e08d485.jpg",
     favorite: false,
   },
   {
@@ -43,7 +43,7 @@ const games = [
       "https://cdn2.steamgriddb.com/file/sgdb-cdn/thumb/00a2c01b0d106174280311c645ffe21f.jpg",
     logo: "https://cdn2.steamgriddb.com/file/sgdb-cdn/logo_thumb/370caceecffaa11d5c0fe2dfefa413c1.png",
     background:
-      "https://cdn2.steamgriddb.com/file/sgdb-cdn/hero_thumb/39ecc5282d4511dca4c757113e1312d1.jpg",
+      "https://cdn2.steamgriddb.com/file/sgdb-cdn/hero/39ecc5282d4511dca4c757113e1312d1.jpg",
     favorite: false,
   },
 ];
