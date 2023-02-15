96aefb377bb865735b1f82850061512a0e0f2150
Alfredo Cruz <arandevs22@gmail.com>
2023-02-15 05:30:56 -0600

    cors config

--- index.js
+++ index.js
@@ -85,5 +85,16 @@ app.delete("/api/games/:id", (req, res) => {
   res.send(game);
 });
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  res.header("Allow", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
+
 const PORT = process.env.PORT || 3197;
 app.listen(PORT, () => console.log(`Escuchando en el puerto ${PORT}`));
