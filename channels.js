61cb62f30fae307671c55c1fc9fabc6e8be110de
alfredo cruz <arandevs22@gmail.com>
2023-03-02 01:24:47 -0600

    channels commit

--- channels.js
+++ channels.js
@@ -5,7 +5,63 @@ let channels = [
         reference: "go:tv01",
         genre: "entretenimiento",
         logo: "https://raw.githubusercontent.com/arandevs22/hoo_play/master/img/logos/telemundo.jpg"
-    }
+    },
+    {
+        id: 2,
+        title: "Las Estrellas",
+        reference: "go:tv02",
+        genre: "entretenimiento",
+        logo: "https://raw.githubusercontent.com/arandevs22/hoo_play/master/img/logos/las_estrellas.jpg"
+    },
+    {
+        id: 3,
+        title: "Azteca 7",
+        reference: "go:tv03",
+        genre: "entretenimiento",
+        logo: "https://raw.githubusercontent.com/arandevs22/hoo_play/master/img/logos/azteca_siete.jpg"
+    },
+    {
+        id: 4,
+        title: "Bandamax",
+        reference: "go:tv04",
+        genre: "musica",
+        logo: "https://raw.githubusercontent.com/arandevs22/hoo_play/master/img/logos/bandamax.jpg"
+    },
+    {
+        id: 5,
+        title: "BitMe",
+        reference: "go:tv05",
+        genre: "infantiles",
+        logo: "https://raw.githubusercontent.com/arandevs22/hoo_play/master/img/logos/bitme.jpg"
+    },
+    {
+        id: 6,
+        title: "Cartoonito",
+        reference: "go:tv06",
+        genre: "infantiles",
+        logo: "https://raw.githubusercontent.com/arandevs22/hoo_play/master/img/logos/cartoonito.jpg"
+    },
+    {
+        id: 7,
+        title: "Cartoon Network",
+        reference: "go:tv07",
+        genre: "infantiles",
+        logo: "https://raw.githubusercontent.com/arandevs22/hoo_play/master/img/logos/cartoon_network.jpg"
+    },
+    {
+        id: 8,
+        title: "FX",
+        reference: "go:tv08",
+        genre: "peliculas",
+        logo: "https://raw.githubusercontent.com/arandevs22/hoo_play/master/img/logos/fx.jpg"
+    },
+    {
+        id: 9,
+        title: "ADN 40",
+        reference: "go:tv09",
+        genre: "peliculas",
+        logo: "https://raw.githubusercontent.com/arandevs22/hoo_play/master/img/logos/fx.jpg"
+    },
 ]
 
 module.exports.channels = channels;
