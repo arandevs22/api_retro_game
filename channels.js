98a842fa53c138a522dab698b852ed7002448704
alfredo cruz <arandevs22@gmail.com>
2023-03-02 13:21:12 -0600

    channels commit

--- channels.js
+++ channels.js
@@ -59,9 +59,157 @@ let channels = [
         id: 9,
         title: "ADN 40",
         reference: "go:tv09",
-        genre: "peliculas",
+        genre: "noticias",
         logo: "https://raw.githubusercontent.com/arandevs22/hoo_play/master/img/logos/adn_40.jpg"
     },
+    {
+        id: 10,
+        title: "Antena 3",
+        reference: "go:tv10",
+        genre: "entretenimiento",
+        logo: "https://raw.githubusercontent.com/arandevs22/hoo_play/master/img/logos/anterna_3.jpg"
+    },
+    {
+        id: 11,
+        title: "AXN",
+        reference: "go:tv11",
+        genre: "entretenimiento",
+        logo: "https://raw.githubusercontent.com/arandevs22/hoo_play/master/img/logos/axn.jpg"
+    },
+    {
+        id: 12,
+        title: "Azteca UNO",
+        reference: "go:tv12",
+        genre: "entretenimiento",
+        logo: "https://raw.githubusercontent.com/arandevs22/hoo_play/master/img/logos/azteca_uno.jpg"
+    },
+    {
+        id: 13,
+        title: "Animal Planet",
+        reference: "go:tv13",
+        genre: "cultura",
+        logo: "https://raw.githubusercontent.com/arandevs22/hoo_play/master/img/logos/animal_planet.jpg"
+    },
+    {
+        id: 14,
+        title: "Cinecanal",
+        reference: "go:tv14",
+        genre: "peliculas",
+        logo: "https://raw.githubusercontent.com/arandevs22/hoo_play/master/img/logos/cinecanal.jpg"
+    },
+    {
+        id: 15,
+        title: "Discovery Channel",
+        reference: "go:tv15",
+        genre: "cultura",
+        logo: "https://raw.githubusercontent.com/arandevs22/hoo_play/master/img/logos/discovery_channel.jpg"
+    },
+    {
+        id: 16,
+        title: "Disney Jr",
+        reference: "go:tv16",
+        genre: "infantiles",
+        logo: "https://raw.githubusercontent.com/arandevs22/hoo_play/master/img/logos/disney_jr.jpg"
+    },
+    {
+        id: 17,
+        title: "Star Channel",
+        reference: "go:tv17",
+        genre: "entretenimiento",
+        logo: "https://raw.githubusercontent.com/arandevs22/hoo_play/master/img/logos/star_channel.jpg"
+    },
+    {
+        id: 18,
+        title: "Golden",
+        reference: "go:tv18",
+        genre: "peliculas",
+        logo: "https://raw.githubusercontent.com/arandevs22/hoo_play/master/img/logos/golden.jpg"
+    },
+    {
+        id: 19,
+        title: "History Channel",
+        reference: "go:tv19",
+        genre: "cultura",
+        logo: "https://raw.githubusercontent.com/arandevs22/hoo_play/master/img/logos/cultura.jpg"
+    },
+    {
+        id: 20,
+        title: "Imagen TV",
+        reference: "go:tv20",
+        genre: "entretenimiento",
+        logo: "https://raw.githubusercontent.com/arandevs22/hoo_play/master/img/logos/imagen_tv.jpg"
+    },
+    {
+        id: 21,
+        title: "Milenio Television",
+        reference: "go:tv21",
+        genre: "noticias",
+        logo: "https://raw.githubusercontent.com/arandevs22/hoo_play/master/img/logos/milenio_television.jpg"
+    },
+    {
+        id: 22,
+        title: "Nat Geo",
+        reference: "go:tv22",
+        genre: "cultura",
+        logo: "https://raw.githubusercontent.com/arandevs22/hoo_play/master/img/logos/nat_geo.jpg"
+    },
+    {
+        id: 23,
+        title: "Teen Nick",
+        reference: "go:tv23",
+        genre: "infantiles",
+        logo: "https://raw.githubusercontent.com/arandevs22/hoo_play/master/img/logos/teen_nick.jpg"
+    },
+    {
+        id: 24,
+        title: "Nick Jr",
+        reference: "go:tv24",
+        genre: "infantiles",
+        logo: "https://raw.githubusercontent.com/arandevs22/hoo_play/master/img/logos/nick_jr.jpg"
+    },
+    {
+        id: 25,
+        title: "Nick",
+        reference: "go:tv25",
+        genre: "infantiles",
+        logo: "https://raw.githubusercontent.com/arandevs22/hoo_play/master/img/logos/nick.jpg"
+    },
+    {
+        id: 26,
+        title: "Paramount",
+        reference: "go:tv26",
+        genre: "peliculas",
+        logo: "https://raw.githubusercontent.com/arandevs22/hoo_play/master/img/logos/paramount.jpg"
+    },
+    {
+        id: 27,
+        title: "Tooncast",
+        reference: "go:tv27",
+        genre: "infantiles",
+        logo: "https://raw.githubusercontent.com/arandevs22/hoo_play/master/img/logos/tooncast.jpg"
+    },
+    {
+        id: 28,
+        title: "Sony Channel",
+        reference: "go:tv28",
+        genre: "entretenimiento",
+        logo: "https://raw.githubusercontent.com/arandevs22/hoo_play/master/img/logos/sony_channel.jpg"
+    },
+    {
+        id: 29,
+        title: "Warnel",
+        reference: "go:tv29",
+        genre: "entretenimiento",
+        logo: "https://raw.githubusercontent.com/arandevs22/hoo_play/master/img/logos/warner.jpg"
+    },
+    {
+        id: 30,
+        title: "Multimedios",
+        reference: "go:tv30",
+        genre: "entretenimiento",
+        logo: "https://raw.githubusercontent.com/arandevs22/hoo_play/master/img/logos/multimedios.jpg"
+    },
+
 ]
 
 module.exports.channels = channels;
