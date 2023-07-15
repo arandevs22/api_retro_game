448de998f2ddcb28c9a6c5f0a0594f9cf62d92ee
freddy <arandevs22@gmail.com>
2023-07-14 23:49:40 -0500

    update commit

--- movies.js
+++ movies.js
@@ -2,8 +2,30 @@ let movies = [
     {
         id: 1119173,
         title: "Una vida maravillosa",
+        year: 2023,
         genre: "Drama",
-        poster_path: "kcDL57ZP833yQLC4kIqNpRziWOF.jpg" 
+        poster_path: "kcDL57ZP833yQLC4kIqNpRziWOF.jpg", 
+    },
+    {
+        id: 1085103,
+        title: "Sin tiempo",
+        year: 2023,
+        genre: "Terror",
+        poster_path: "bkPpmNSdbRIgtscqH86BQFLJXcz.jpg"
+    },
+    {
+        id: 787723,
+        title: "En la tormenta",
+        year: 2021,
+        genre: "Suspenso",
+        poster_path: "e8P30DE8TsVam6jlCSBpfO0CizG.jpg"
+    },
+    {
+        id: 785538,
+        title: "7 Prisioneros",
+        year: "2021",
+        genre: "Drama",
+        poster_path: "5dCIBGXnIuPdP1gmlElEDMTcIGT.jpg"
     }
 ]
 
